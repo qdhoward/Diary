046b6ca371ff4eff324012877a355c272cad8c43
Hao Yu <howardyu_cn@outlook.com>
2018-10-26 00:09:39 -0400

    bfs1

---
+++ NewAutumn/refresh/BreadthFS/KeysInBTLayerByLayer.java
@@ -0,0 +1,36 @@
+package NewAutumn.refresh.BreadthFS;
+
+import util.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class KeysInBTLayerByLayer {
+    public List<List<Integer>> layerByLayer(TreeNode root) {
+        // Write your solution here.
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()) {
+            List<Integer> curLayer = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = queue.poll();
+                curLayer.add(cur.key);
+                if (cur.left != null) {
+                    queue.offer(cur.left);
+                }
+                if (cur.right != null) {
+                    queue.offer(cur.right);
+                }
+            }
+            res.add(curLayer);
+        }
+        return res;
+    }
+}
--- NewAutumn/refresh/Heap/KSmallestInUnsortedArray.java
+++ NewAutumn/refresh/Heap/KSmallestInUnsortedArray.java
@@ -1,5 +1,6 @@
 package NewAutumn.refresh.Heap;
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Queue;
@@ -18,8 +19,68 @@ public class KSmallestInUnsortedArray {
                 return o1 > o2 ? -1 : 1;
             }
         });
-        for (int i = 0; i < k; i++) {
-            maxHeap.offer()
+        for (int i = 0; i < array.length; i++) {
+            if (i < k) {
+                maxHeap.offer(array[i]);
+            } else {
+                if (array[i] < maxHeap.peek()) {
+                    maxHeap.poll();
+                    maxHeap.offer(array[i]);
+                }
+            }
+
+        }
+        int[] result = new int[k];
+        for (int i = k - 1; i > 0; i--) {
+            result[i] = maxHeap.poll();
+        }
+        return result;
+    }
+
+    public int[] kSmallest2(int[] array, int k) {
+        if (array == null || array.length == 0) {
+            return new int[0];
+        }
+        quickselect(array, 0, array.length, k);
+        int[] result = Arrays.copyOf(array, k);
+        Arrays.sort(result);// It may not be necessary.
+        return result;
+    }
+    private void quickselect(int[] array, int left, int right, int targetIndex) {
+        int mid = partition(array, left, right);
+        if (mid == targetIndex) {
+            return;
+        } else if (mid < targetIndex) {
+            quickselect(array, mid + 1, right, targetIndex);
+        } else {
+            quickselect(array, left, mid - 1, targetIndex);
         }
     }
+
+    private int partition(int[] array, int left, int right) {
+        int pivotIndex = left + (int) (Math.random() * (right - left + 1));
+        int pivot = array[pivotIndex];
+        int start = left;
+        int end = right - 1;
+        swap(array, pivotIndex, right);
+        while (start <= end) {
+            if (array[start] < pivot) {
+                start++;
+            } else if (array[end] >= pivot) {
+                end--;
+            } else {
+                swap(array, start++, end--);
+            }
+        }
+        swap(array, pivotIndex, right);
+        return pivotIndex;
+    }
+
+    private void swap(int[] array, int left, int right) {
+        int tmp = array[left];
+        array[left] = array[right];
+        array[right] = tmp;
+    }
+
+
 }
