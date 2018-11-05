1688624b728a11e2376e80226c87ba97760b5e97
qdhoward <howardyu_cn@outlook.com>
2018-11-05 11:19:35 -0500

    merge

--- NewAutumn/topic/ConnectivityDFSAndBFS1/index.java
+++ NewAutumn/topic/ConnectivityDFSAndBFS1/index.java
@@ -14,7 +14,7 @@ public class index {
         int[][] maze2 = new int[][] {
                 new int[] {1, 1, 1, 1, 1, 1},
                 new int[] {1, 0, 0, 0, 0, 1},
-                new int[] {1, 0, 0, 1, 0, 1},
+                new int[] {1, 0, 0, 0, 0, 1},
                 new int[] {1, 0, 0, 1, 0, 1},
                 new int[] {1, 0, 0, 1, 0, 1},
                 new int[] {1, 1, 1, 1, 1, 1},
