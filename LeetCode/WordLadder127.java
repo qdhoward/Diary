1c3fd7482e90697a519bf0f3b1852553b0f828b0
Hao Yu <howardyu_cn@outlook.com>
2018-11-14 21:33:00 -0500

    dp first try!!

--- LeetCode/WordLadder127.java
+++ LeetCode/WordLadder127.java
@@ -1,9 +1,35 @@
 package LeetCode;
 
-import java.util.List;
+import java.util.*;
 
 public class WordLadder127 {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Queue<String> queue = new LinkedList<>();
+        Set<String> words = new HashSet<>(wordList);
+        queue.offer(beginWord);
+        int step = 1;
+        while (!queue.isEmpty()) {
+            step++;
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                char[] cur = queue.poll().toCharArray();
+                for (int j = 0; j < cur.length; j++) {
+                    for (char k = 'a'; k <= 'z'; k++) {
+                        char tmp = cur[j];
+                        cur[j] = k;
+                        String curStr = new String(cur);
+                        if (curStr.equals(endWord) && words.contains(endWord)) {
+                            return step;
+                        }
+                        if (words.contains(curStr)) {
+                            queue.offer(curStr);
+                            words.remove(curStr);
+                        }
+                        cur[j] = tmp;
+                    }
+                }
+            }
+        }
         return 0;
     }
 }
--- LeetCode/index.java
+++ LeetCode/index.java
@@ -6,8 +6,14 @@ public class index {
     public static void main(String[] args) {
         WordBreak139 test = new WordBreak139();
         List<String> a = new ArrayList<String>();
-        a.add("apple");
-        a.add("pen");
-        System.out.println(test.wordBreak("applepenapple", a));
+//        a.add("apple");
+//        a.add("pen");
+        WordLadder127 test2 = new WordLadder127();
+        a.add("hot");
+        a.add("dot");
+        a.add("dog");
+        a.add("lot");
+        a.add("log");
+        System.out.println(test2.ladderLength("hit", "cog", a));
     }
 }
