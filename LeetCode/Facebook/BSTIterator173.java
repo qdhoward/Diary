45297ec12e603428e3d06c37fc397502b358e67f
Hao Yu <howardyu_cn@outlook.com>
2018-12-20 00:20:34 -0500

    2018 12 19 6道

---
+++ LeetCode/Facebook/BSTIterator173.java
@@ -0,0 +1,37 @@
+package LeetCode.Facebook;
+
+import util.TreeNode;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class BSTIterator173 {
+    private Deque<TreeNode> stack;
+    public BSTIterator173(TreeNode root) {
+        stack = new LinkedList<>();
+        TreeNode cur = root;
+        while (cur != null) {
+            stack.push(cur);
+            cur = cur.left;
+        }
+    }
+
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+
+    /** @return the next smallest number */
+    public int next() {
+        TreeNode next = stack.pop();
+        TreeNode cur = next;
+        if (cur.right != null) {
+            cur = cur.right;
+            while (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            }
+        }
+        return next.val;
+    }
+}
